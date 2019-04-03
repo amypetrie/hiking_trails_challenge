7ff97de43648c27338f2d54c44f69c6ee80c35e0
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-04-03 16:47:18 -0600

    Update database cleaner config to clean/reset IDs during testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'database_cleaner'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -25,17 +24,13 @@ require 'database_cleaner'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+
+ActiveRecord::Migration.maintain_test_schema!
 
 DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |config|
-  config.before(:all) do
+  config.before(:each) do
     DatabaseCleaner.clean
   end
   config.after(:each) do
